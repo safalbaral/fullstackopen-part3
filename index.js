cc83fed6c46fcc04b962b93dc8eb8e2ba2e184e7
Safal Baral <130429214+safalbaral@users.noreply.github.com>
2024-06-08 10:38:26 +0545

    Completed upto 3.8

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 let entries = [
     { 
