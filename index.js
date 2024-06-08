ba5f0c578c32f728556b345f9462ce4abee9bf0b
Safal Baral <130429214+safalbaral@users.noreply.github.com>
2024-06-08 09:38:33 +0545

    Completed upto 3.6

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express()
+app.use(express.json())
 
 let entries = [
     { 
@@ -25,8 +26,46 @@ let entries = [
     }
 ]
 
+const generateRandomId = () => {
+    return Math.floor(Math.random() * 1000000);
+  }
+
 app.get('/api/persons', (request, response) => {
     response.json(entries)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const entry = entries.find(entry => entry.id === id)
+    entry ? response.json(entry) : response.status(404).end()
+})
+
+app.post('/api/persons', (request, response) => {  
+    const entry = request.body //This is made only possible by the json middleware   
+    if (entries.find(e => e.name === entry.name)) {
+        response.status(403).json({
+            error: 'Name must be unique'
+        })
+    } else if (entry.name === '') {
+        response.status(400).json({
+            error: 'The name field must not be blank'
+        }
+        )
+    } else {
+        entry.id = generateRandomId()
+        entries = entries.concat(entry)
+        response.json(entry)
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    entries = entries.filter(entry => entry.id !== id)
+    response.status(204).end()
+})
+
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${entries.length} people. <br /> ${Date()}`)
 })
 
 const PORT = 3001
