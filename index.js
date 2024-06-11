c732153136fc09accdf804940e0b4a90327c7853
Safal Baral <safalbaral@Safals-MacBook-Pro.local>
2024-06-11 18:01:34 +0545

    Push to get app in order for deployment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -72,7 +74,7 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${entries.length} people. <br /> ${Date()}`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log('Server running on port', PORT)
