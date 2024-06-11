bed0fc8e9583ecbd9ca0e90f26395e7af5414252
Safal Baral <safalbaral@Safals-MacBook-Pro.local>
2024-06-11 19:09:50 +0545

    Ready for deployment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -35,7 +36,7 @@ let entries = [
 const generateRandomId = () => {
     return Math.floor(Math.random() * 1000000);
   }
-
+  
 app.get('/api/persons', (request, response) => {
     response.json(entries)
 })
