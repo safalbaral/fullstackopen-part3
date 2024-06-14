e7c213162aaa77b2bd05abead23af73a7df2c288
Safal Baral <safalbaral@Safals-MacBook-Pro.local>
2024-06-14 15:11:03 +0545

    Completed 3.22

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
+
+export default [
+  {
+    ignores: ['dist']
+  },
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+      ],
+      'no-console': 'off',
+      '@stylistic/js/indent': [
+        'error',
+        2
+      ],
+      '@stylistic/js/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/js/quotes': [
+        'error',
+        'single'
+      ],
+      '@stylistic/js/semi': [
+        'error',
+        'never'
+      ]
+    }
+  },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended
+]
--- models/entry.js
+++ models/entry.js
@@ -6,27 +6,27 @@ const url = process.env.MONGODB_URI
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('Connected to MongoDB!')
-    })
-    .catch(error => {
-        console.log('Error connecting to MongoDB: ', error.message)
-    })
+  .then(result => {
+    console.log('Connected to MongoDB!')
+  })
+  .catch(error => {
+    console.log('Error connecting to MongoDB: ', error.message)
+  })
 
 const entrySchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
     required: true
-},
+  },
   number: {
     type: String,
     required: true,
     minLength: 8,
     validate: {
-        validator: (v) => {
-            return /^\d{2,3}-\d+$/.test(v)
-        }
+      validator: (v) => {
+        return /^\d{2,3}-\d+$/.test(v)
+      }
     }
   }
 })
@@ -34,11 +34,11 @@ const entrySchema = new mongoose.Schema({
 // id may look like a string, but it's an object so we turn it into a string
 // And we also remove the __v attribute when passing to the frontend
 entrySchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = document._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = document._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Entry', entrySchema)
