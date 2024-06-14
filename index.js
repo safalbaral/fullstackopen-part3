fdcdca6f22cdfebf9528edd53ea9c5f6f42fbfc8
Safal Baral <130429214+safalbaral@users.noreply.github.com>
2024-06-14 15:11:03 +0545

    Completed 3.22

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
@@ -17,108 +17,104 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 
 let entries = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    'id': 1,
+    'name': 'Arto Hellas',
+    'number': '040-123456'
+  },
+  {
+    'id': 2,
+    'name': 'Ada Lovelace',
+    'number': '39-44-5323523'
+  },
+  {
+    'id': 3,
+    'name': 'Dan Abramov',
+    'number': '12-43-234345'
+  },
+  {
+    'id': 4,
+    'name': 'Mary Poppendieck',
+    'number': '39-23-6423122'
+  }
 ]
 
-const generateRandomId = () => {
-    return Math.floor(Math.random() * 1000000);
-  }
-  
 app.get('/api/persons', (request, response) => {
-    Entry.find({}).then(entries => {
-        response.json(entries)
-    })
+  Entry.find({}).then(entries => {
+    response.json(entries)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    Entry.findById(request.params.id).then(entry => {
-        if (entry){
-            response.json(entry)
-        } else {
-            response.status(404).end()
-        }
-    })
+  Entry.findById(request.params.id).then(entry => {
+    if (entry){
+      response.json(entry)
+    } else {
+      response.status(404).end()
+    }
+  })
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response, next) => {  
-    const body = request.body //This is made only possible by the json middleware   
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body //This is made only possible by the json middleware
 
-    const entry = new Entry({
-        name: body.name,
-        number: body.number
-    })
+  const entry = new Entry({
+    name: body.name,
+    number: body.number
+  })
 
-    entry.save().then(savedEntry => {
-        response.json(savedEntry)
-    }).catch(error => next(error))
+  entry.save().then(savedEntry => {
+    response.json(savedEntry)
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Entry.findByIdAndDelete(request.params.id)
+  Entry.findByIdAndDelete(request.params.id)
     .then(result => {
-        response.status(204).end()
+      response.status(204).end()
     })
     .catch(error => next(error))
 
-    const id = Number(request.params.id);
-    entries = entries.filter(entry => entry.id !== id)
-    response.status(204).end()
+  const id = Number(request.params.id)
+  entries = entries.filter(entry => entry.id !== id)
+  response.status(204).end()
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const entry = {
-        name: body.name,
-        number: body.number
-    }
+  const entry = {
+    name: body.name,
+    number: body.number
+  }
 
-    Entry.findByIdAndUpdate(request.params.id, entry, {new: true, runValidators: true, context: 'query'}) // {new:true} ensures that the entry provided to the callback funciton is the updated entry and not the old one, which is the default behavior
-        .then(updatedEntry => {
-            response.json(updatedEntry)
-        }).catch(error => next(error))
+  Entry.findByIdAndUpdate(request.params.id, entry, { new: true, runValidators: true, context: 'query' }) // {new:true} ensures that the entry provided to the callback funciton is the updated entry and not the old one, which is the default behavior
+    .then(updatedEntry => {
+      response.json(updatedEntry)
+    }).catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${entries.length} people. <br /> ${Date()}`)
+  response.send(`Phonebook has info for ${entries.length} people. <br /> ${Date()}`)
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
+  console.log(error.message)
 
-    console.log('ERROR NAME IS', error.name)
+  console.log('ERROR NAME IS', error.name)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({
-            error: 'Malformatted ID'
-        })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({
-            error: error.message
-        })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({
+      error: 'Malformatted ID'
+    })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.message
+    })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
@@ -126,5 +122,5 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-    console.log('Server running on port', PORT)
+  console.log('Server running on port', PORT)
 })
--- mongo.js
+++ mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 if (process.argv.length<3) {
   console.log('give password as argument')
   process.exit(1)
-} 
+}
 
 const password = process.argv[2]
 
@@ -22,21 +22,21 @@ const entrySchema = new mongoose.Schema({
 const Entry = mongoose.model('Entry', entrySchema)
 
 if (process.argv.length === 3) {
-    Entry.find({}).then(result => {
-        result.forEach(entry => {
-            console.log(entry)
-            mongoose.connection.close()
-            process.exit(0)
-        })
+  Entry.find({}).then(result => {
+    result.forEach(entry => {
+      console.log(entry)
+      mongoose.connection.close()
+      process.exit(0)
     })
+  })
 } else {
-    const entry = new Entry({
+  const entry = new Entry({
     name: process.argv[3],
     number: process.argv[4],
-    })
+  })
 
-    entry.save().then(result => {
+  entry.save().then(result => {
     console.log(`Added ${result.name} number ${result.number} to phonebook`)
     mongoose.connection.close()
-})
+  })
 }
